3279718b8010b5de6f66440b5029ff2f72424a78
EbraamSobhy <ibraam.sobhy2003@gmail.com>
2026-05-05 23:53:12 +0300

    changes

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create needed dirs
+mkdir -p ~/.config
+
+# Symlinks
+ln -sf ~/dotfiles/bash/.bashrc ~/.bashrc
+ln -sf ~/dotfiles/nvim ~/.config/nvim
+ln -sf ~/dotfiles/tmux/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/i3/config ~/.config/i3/config
+
+echo "Dotfiles installed!"
